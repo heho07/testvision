a3fbd37fcab5e9f9d933c58f0d4af80fa6799062
Herman <hermanor@kth.se>
2019-04-09 10:40:25 +0200

    Updates

---
+++ precache-manifest.c8fde6d73e3264faf6f8601983466305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testvision/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02c5ca176cf7cb416322",
+    "url": "/testvision/static/js/runtime~main.b5a8dad5.js"
+  },
+  {
+    "revision": "f1a50fa558a221e43c6e",
+    "url": "/testvision/static/js/main.11df3bef.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/testvision/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f1a50fa558a221e43c6e",
+    "url": "/testvision/static/css/main.75741766.chunk.css"
+  },
+  {
+    "revision": "eba57bcea9cd32a9d36d07b393cda632",
+    "url": "/testvision/index.html"
+  }
+];
